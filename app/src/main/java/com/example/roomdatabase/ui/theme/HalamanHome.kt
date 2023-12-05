1b5b743979d843fadbdf30d2d1cf3b3e5fdffe36
FakriSouyo <fakriggwp@gmail.com>
2023-12-05 23:00:10 +0700

    Home

--- app/src/main/java/com/example/roomdatabase/ui/theme/HalamanHome.kt
+++ app/src/main/java/com/example/roomdatabase/ui/theme/HalamanHome.kt
@@ -3,6 +3,7 @@ package com.example.roomdatabase.ui.theme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,6 +22,7 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomdatabase.R
 import com.example.roomdatabase.data.Siswa
 import com.example.roomdatabase.model.HomeViewModel
 import com.example.roomdatabase.model.PenyediaViewModel
@@ -85,6 +87,25 @@ fun BodyHome(
             ListSiswa(
                 itemSiswa = itemSiswa,
                 modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ListSiswa(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier=Modifier
+){
+    LazyColumn(modifier = Modifier){
+        items(
+            items = itemSiswa, key = {it.id}){
+                person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
             )
         }
     }
