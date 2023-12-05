b0052ca28e18879a8888f821c86c92079495a5d2
FakriSouyo <fakriggwp@gmail.com>
2023-12-05 22:42:49 +0700

    update package Halaman

---
+++ app/src/main/java/com/example/roomdatabase/model/EntryViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.roomdatabase.model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.repositori.RepositoriSiswa
+
+
+class EntryViewModel(private val repositorySiswa: RepositorySiswa): ViewModel(){
+
+    /**
+     * Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di -entry */
+    suspend fun saveSiswa(){
+        if (validasiInput()){
+            repositorySiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
